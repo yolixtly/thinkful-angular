cba09ea4a73a4a499d03ae3591da93fb72ba06b9
yolixtly <yolixtly_13@hotmail.com>
2016-10-11 16:09:17 -0400

    ng-transculde

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-angular.module('myApp', []);
+var app = angular.module('myApp', []);
+
+app.directive('yoYo', function(){
+	return {
+		restrict: 'E',
+        transclude: true,
+        template: '<div class="yo-yo">Yo <span ng-transclude></span>!</div>',
+        replace: true
+	};
+});
